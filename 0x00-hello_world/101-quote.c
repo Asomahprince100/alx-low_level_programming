9d36190da5d5fab76a41e35ab7ca095630045cf5
ASOMAH PRINCE MOUNT <asomahprince99@gmail.com>
2023-03-16 22:09:02 -0700

    This is 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - This is a C programm
+ *
  * Return 1 (successful)
  */
+
 int main (void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
