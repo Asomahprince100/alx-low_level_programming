da9d494456b757a2b3b73645fbcd7a221e6b5248
ASOMAH PRINCE MOUNT <asomahprince99@gmail.com>
2023-03-17 21:36:47 -0700

    This is 0-positive_or_negative.c

--- alx-low_level_programming/0-positive_or_negative.c
+++ alx-low_level_programming/0-positive_or_negative.c
@@ -1,27 +1,32 @@
+
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
-/**
- * main - This is the main function
- *
- * Return: This is the return 0
- */
 
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive", n);
+	{
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-		printf("%d is zero", n);
-	else 
-		printf("%d is negative", n);
-	printf("\n");
-	return  (0);
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d Is negative\n", n);
+	}
+	return (0);
 }
+
+
