4379f791e7d3347eff0519d8912aa33c8554e125
ASOMAH PRINCE MOUNT <asomahprince99@gmail.com>
2023-04-12 08:49:25 -0700

    This is putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 	/**
-	 * main - print _putchar
+	 * main - This prints putchar.c
+	 *
 	 * is written by mbah
 	 * Return: 0
 	 */
@@ -8,14 +9,14 @@
 
 	int main(void)
 	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
 	}
